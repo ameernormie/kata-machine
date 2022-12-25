c3c537367dd4f56914cc5f3e9df704428469bca7
Ameer Hamza <ameernormie@gmail.com>
2022-12-25 18:35:05 +0500

    feat: BubbleSort code

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,16 @@
+/**
+ * We are going over length of the array, and everytime we go over it, we go over some or whole of the array
+ *
+ * @param arr
+ */
 export default function bubble_sort(arr: number[]): void {
-
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
